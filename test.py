9c1e43f9f60d63e965ec74339b4020792783ef33
ssyoii <ssyoiw@gmail.com>
2020-10-28 22:39:04 +0900

    new direction

--- test.py
+++ test.py
@@ -1,21 +1,11 @@
+import os
 import pandas as pd
+from glob import glob
 
-df01 = pd.read_csv("test01.csv")
-df02 = pd.read_csv("test02.csv")
+files = glob('data/*.csv')
 
-df01 = df01.add_prefix("1_")
-df02 = df02.add_prefix("2_")
+list = []
+for f in files:
+    list.append(pd.read_csv(f, header=1, names=("X", "Y", "Z", "Volume Fraction")))
 
-df01 = df01.drop(["1_y", "1_z"], axis=1)
-df02 = df02.drop(["2_y", "2_z"], axis=1)
-
-result = df01.join(df02)
-
-ave = result.mean(axis=1)
-ave = pd.DataFrame(ave, columns=["X"])
-
-print("Hello")
-print(result)
-print(ave)
-
-ave.to_csv("average.csv", index = False)
+print(list)
