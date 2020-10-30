a31fc956ff60dd72ceaab8e60fd38889ea4191af
ssyoii <ssyoiw@gmail.com>
2020-10-30 19:31:25 +0900

    update

--- test.py
+++ test.py
@@ -8,4 +8,17 @@ list = []
 for f in files:
     list.append(pd.read_csv(f, header=1, names=("X", "Y", "Z", "Volume Fraction")))
 
+for i in range(len(list)):
+    list[i] = list[i].drop(["X","Y","Z"], axis=1)
+    if i == 0:
+        df = list[0]
+    else:
+        df += list[i]
+    print(df)
+
+result = df / len(list)
+
+result.to_excel("data/average.xlsx")
+    
+
 print(list)
